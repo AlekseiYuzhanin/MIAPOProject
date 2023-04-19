d85b7443bfb2b10ede60e7442db408aee043009d
Aleksey <ZombeHalk@gmail.com>
2023-04-19 16:20:44 +0700

    commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct{
+	ID string `json:id`
+	Isbn string `json:isbn`
+	Title string `json:title`
+	Director *Director `json:director`
+}
+
+type Director struct{
+	Firstname string `json:firstname`
+	Lastname string `json:lastname`
+}
+
+var movies []Movie
+
+func getMovie(w http.ResponseWriter, r*http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _,item := range movies{
+		if item.ID == params["id"]{
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func main(){
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movie",getMovies).Methods("GET")
+	r.HandleFunc("/movies{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies",createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}",deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Server started at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000",r))
+}
